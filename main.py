0580cb76392c33b552e1581de2bebd9295f6afce
jorgeav527 <jorgeav527@gmail.com>
2025-06-24 20:42:07 -0500

    feat: add index.html style.css, mod: main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-x = 5
-y = 8
-z = 20
-q = 90
-w = 50
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
