fb4bd163ff54e7200b97e8aea1bf72d0e53559d5
jorgeav527 <jorgeav527@gmail.com>
2025-06-24 21:07:41 -0500

    feat: hemos agragado mas sintasis de jinja2

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def base():
+    return render_template('base.html')
+
+@app.route('/home', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
